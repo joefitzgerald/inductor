8bc1bebc5d5bb7f7eb517c5f9b40e068a13855a8
Shawn Neal <sneal@sneal.net>
2016-02-04 14:56:39 -0800

    Refactor test duplication

--- osregistry/operating_system_test.go
+++ osregistry/operating_system_test.go
@@ -27,94 +27,86 @@ var testData = `
 `
 
 func TestCanListAllOSs(t *testing.T) {
-	registry, err := New(strings.NewReader(testData))
-	if err != nil {
-		t.Error("Failed to load the Windows OS registry:", err)
+	registry := createRegistry(t)
+	os := registry.List()
+	if len(os) != 2 {
+		t.Errorf("Expected 2 OS entries, but got %d instead", len(os))
 	} else {
-		os := registry.List()
-		if len(os) != 2 {
-			t.Errorf("Expected 2 OS entries, but got %d instead", len(os))
-		} else {
-			if os[0] != "windows10" {
-				t.Errorf("Expected the first OS entry to be windows10, but was %s", os[0])
-			}
-			if os[1] != "windows2008r2" {
-				t.Errorf("Expected the second OS entry to be windows2008r2, but was %s", os[0])
-			}
+		if os[0] != "windows10" {
+			t.Errorf("Expected the first OS entry to be windows10, but was %s", os[0])
+		}
+		if os[1] != "windows2008r2" {
+			t.Errorf("Expected the second OS entry to be windows2008r2, but was %s", os[0])
 		}
 	}
 }
 
 func TestCanLoadWindows10Config(t *testing.T) {
-	registry, err := New(strings.NewReader(testData))
-	if err != nil {
-		t.Error("Failed to load the OS registry:", err)
+	registry := createRegistry(t)
+	r, ok := registry.Get("windows10")
+	if !ok {
+		t.Error("Failed to load Windows10 from the OS registry")
 	} else {
-		r, ok := registry.Get("windows10")
-		if !ok {
-			t.Error("Failed to load Windows10 from the OS registry")
-		} else {
-			var tests = []struct {
-				actual   string
-				expected string
-			}{
-				{r.Name, "windows10"},
-				{r.IsoURL, "http://care.dlservice.microsoft.com/dl/download/C/3/9/C399EEA8-135D-4207-92C9-6AAB3259F6EF/10240.16384.150709-1700.TH1_CLIENTENTERPRISEEVAL_OEMRET_X64FRE_EN-US.ISO"},
-				{r.IsoChecksum, "56ab095075be28a90bc0b510835280975c6bb2ce"},
-				{r.IsoChecksumType, "sha1"},
-				{r.VirtualboxGuestOsType, "Windows81_64"},
-				{r.VmwareGuestOsType, "windows8srv-64"},
-				{r.WindowsImageName, "Windows 10 Enterprise Evaluation"},
-			}
-			for _, ts := range tests {
-				if ts.actual != ts.expected {
-					t.Logf("%#v", r)
-					t.Errorf("Expected \"%s\" but got \"%s\"", ts.expected, ts.actual)
-				}
+		var tests = []struct {
+			actual   string
+			expected string
+		}{
+			{r.Name, "windows10"},
+			{r.IsoURL, "http://care.dlservice.microsoft.com/dl/download/C/3/9/C399EEA8-135D-4207-92C9-6AAB3259F6EF/10240.16384.150709-1700.TH1_CLIENTENTERPRISEEVAL_OEMRET_X64FRE_EN-US.ISO"},
+			{r.IsoChecksum, "56ab095075be28a90bc0b510835280975c6bb2ce"},
+			{r.IsoChecksumType, "sha1"},
+			{r.VirtualboxGuestOsType, "Windows81_64"},
+			{r.VmwareGuestOsType, "windows8srv-64"},
+			{r.WindowsImageName, "Windows 10 Enterprise Evaluation"},
+		}
+		for _, ts := range tests {
+			if ts.actual != ts.expected {
+				t.Logf("%#v", r)
+				t.Errorf("Expected \"%s\" but got \"%s\"", ts.expected, ts.actual)
 			}
 		}
 	}
 }
 
 func TestCanLoadWindows2008R2Config(t *testing.T) {
-	registry, err := New(strings.NewReader(testData))
-	if err != nil {
-		t.Error("Failed to load the OS registry:", err)
+	registry := createRegistry(t)
+	r, ok := registry.Get("windows2008r2")
+	if !ok {
+		t.Error("Failed to load Windows 2008 R2 from the OS registry")
 	} else {
-		r, ok := registry.Get("windows2008r2")
-		if !ok {
-			t.Error("Failed to load Windows 2008 R2 from the OS registry")
-		} else {
-			var tests = []struct {
-				actual   string
-				expected string
-			}{
-				{r.Name, "windows2008r2"},
-				{r.IsoURL, "http://download.microsoft.com/download/7/5/E/75EC4E54-5B02-42D6-8879-D8D3A25FBEF7/7601.17514.101119-1850_x64fre_server_eval_en-us-GRMSXEVAL_EN_DVD.iso"},
-				{r.IsoChecksum, "4263be2cf3c59177c45085c0a7bc6ca5"},
-				{r.IsoChecksumType, "md5"},
-				{r.VirtualboxGuestOsType, "Windows2008_64"},
-				{r.VmwareGuestOsType, "windows7srv-64"},
-				{r.WindowsImageName, "Windows Server 2008 R2 SERVERSTANDARD"},
-			}
-			for _, ts := range tests {
-				if ts.actual != ts.expected {
-					t.Logf("%#v", r)
-					t.Errorf("Expected \"%s\" but got \"%s\"", ts.expected, ts.actual)
-				}
+		var tests = []struct {
+			actual   string
+			expected string
+		}{
+			{r.Name, "windows2008r2"},
+			{r.IsoURL, "http://download.microsoft.com/download/7/5/E/75EC4E54-5B02-42D6-8879-D8D3A25FBEF7/7601.17514.101119-1850_x64fre_server_eval_en-us-GRMSXEVAL_EN_DVD.iso"},
+			{r.IsoChecksum, "4263be2cf3c59177c45085c0a7bc6ca5"},
+			{r.IsoChecksumType, "md5"},
+			{r.VirtualboxGuestOsType, "Windows2008_64"},
+			{r.VmwareGuestOsType, "windows7srv-64"},
+			{r.WindowsImageName, "Windows Server 2008 R2 SERVERSTANDARD"},
+		}
+		for _, ts := range tests {
+			if ts.actual != ts.expected {
+				t.Logf("%#v", r)
+				t.Errorf("Expected \"%s\" but got \"%s\"", ts.expected, ts.actual)
 			}
 		}
 	}
 }
 
 func TestMissingOS(t *testing.T) {
+	registry := createRegistry(t)
+	_, ok := registry.Get("Linux")
+	if ok {
+		t.Error("Should have return !ok for Linux")
+	}
+}
+
+func createRegistry(t *testing.T) *OperatingSystemRegistry {
 	registry, err := New(strings.NewReader(testData))
 	if err != nil {
 		t.Error("Failed to load the OS registry:", err)
-	} else {
-		_, ok := registry.Get("Linux")
-		if ok {
-			t.Error("Should have return !ok for Linux")
-		}
 	}
+	return registry
 }
