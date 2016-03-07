8a46908cb5cf2a735643174f5de6feafaaf5ab22
Shawn Neal <sneal@sneal.net>
2016-03-07 11:21:18 -0800

    Extract app type out of main.go

--- cmd/inductor/main.go
+++ cmd/inductor/main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/codegangsta/cli"
-	"github.com/joefitzgerald/inductor/configuration"
-	"github.com/joefitzgerald/inductor/cpy"
-	"github.com/joefitzgerald/inductor/renderer"
-	"github.com/joefitzgerald/inductor/tpl"
 )
 
 // Version of the CLI
@@ -61,107 +55,11 @@ func newApp() *cli.App {
 			Usage: "Run the VM with a GUI",
 		},
 	}
-	app.Action = run
+	app.Action = newInductorApp(die).Run
 	return app
 }
 
-func run(c *cli.Context) {
-	config, err := loadConfiguration(c)
-	if err != nil {
-		die("Couldn't load the inductor.json configuration file.", err)
-	}
-	opts, err := createRenderOpts(c, config)
-	if err != nil {
-		die(err)
-	}
-	outDir, err := outDir(c, config)
-	if err != nil {
-		die(err)
-	}
-
-	// find all templates
-	cwd, err := os.Getwd()
-	if err != nil {
-		die(err)
-	}
-	templates := tpl.New(cwd, opts.OSName)
-
-	// render all the templates to the output directory
-	renderer := renderer.New(opts, outDir)
-	err = renderer.Render(templates)
-	if err != nil {
-		die(err)
-	}
-
-	// copy over any non-templates to the output directory
-	copier := cpy.New()
-	err = copier.Copy(cwd, outDir)
-	if err != nil {
-		die(err)
-	}
-}
-
-func loadConfiguration(c *cli.Context) (config *configuration.InductorConfiguration, err error) {
-	configFile, err := os.Open(c.String("config"))
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		if cerr := configFile.Close(); cerr != nil {
-			err = cerr
-		}
-	}()
-	return configuration.New(configFile)
-}
-
-func createRenderOpts(c *cli.Context, config *configuration.InductorConfiguration) (*renderer.RenderOptions, error) {
-	var osname string
-	if len(c.Args()) > 0 {
-		osname = c.Args()[0]
-	} else {
-		fmt.Println("Available Operating Systems:")
-		fmt.Println()
-		for _, s := range config.List() {
-			fmt.Println(fmt.Sprintf("  %s", s))
-		}
-		fmt.Println()
-		return nil, errors.New("You must specify an operating system argument")
-	}
-
-	// create the default options set based on the inductor config
-	var edition string
-	if len(c.String("edition")) > 0 {
-		edition = c.String("edition")
-	}
-	opts, err := renderer.NewRenderOptions(osname, edition, config)
-	if err != nil {
-		return nil, err
-	}
-
-	// apply any command line overrides to the options set
-	opts.WindowsUpdates = !c.Bool("skipwindowsupdates")
-	opts.Headless = !c.Bool("gui")
-	if len(c.String("productkey")) > 0 {
-		opts.ProductKey = c.String("productkey")
-	}
-	if c.Bool("ssh") {
-		opts.Communicator = "ssh"
-	}
-
-	return opts, err
-}
-
-func outDir(c *cli.Context, config *configuration.InductorConfiguration) (string, error) {
-	outDir := config.OutDir
-	if len(c.String("outdir")) > 0 {
-		outDir = c.String("outdir")
-	}
-	return filepath.Abs(outDir)
-}
-
-func die(vals ...interface{}) {
-	if len(vals) > 1 || vals[0] != nil {
-		fmt.Fprintf(os.Stderr, fmt.Sprintln(vals...))
-		os.Exit(1)
-	}
+func die(err error) {
+	fmt.Fprintf(os.Stderr, "%s\n", err)
+	os.Exit(1)
 }
